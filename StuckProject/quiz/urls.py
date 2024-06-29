0ae3edbec80067341d25ae84d169a3f8f9b6c356
7beunseo <kimes0403@gmail.com>
2024-06-30 01:38:48 +0900

    Feat: 드래그로 폴더 이동

--- StuckProject/quiz/urls.py
+++ StuckProject/quiz/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', home, name="home"),
     path('category/<int:folder_id>/', view_folder, name='folder-view'),
     path('category/add/<int:parent_id>/', add_folder, name='add-folder'),
+    path('folder/move/<int:folder_id>/to/<int:parent_id>/', move_folder, name='move_category'),
 ]
--- StuckProject/quiz/views.py
+++ StuckProject/quiz/views.py
@@ -37,3 +37,21 @@ def add_folder(request, parent_id):
     if parent == None:
         return redirect('folder-view', 0)
     return redirect('folder-view', parent.id)
+
+
+# 폴더 드래그로 이동
+def move_folder(request, folder_id, parent_id):
+    folder = get_object_or_404(Folder, id=folder_id)
+
+    if parent_id == 0:
+        new_parent = None
+    else:
+        new_parent = get_object_or_404(Folder, id=parent_id)
+
+    folder.parent = new_parent
+    folder.save()
+
+    if new_parent == None:
+            return redirect('folder-view', 0)
+    
+    return redirect('folder-view', folder_id=new_parent.parent.id if new_parent.parent else 0)
