9109c0b47a40d49244f43d5a8b8c8a6b70f0c166
7beunseo <kimes0403@gmail.com>
2024-06-30 01:31:49 +0900

    Feat: 폴더 재귀적으로 생성 및 조회

--- StuckProject/quiz/urls.py
+++ StuckProject/quiz/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name="home")
+    path('', home, name="home"),
+    path('category/<int:folder_id>/', view_folder, name='folder-view'),
+    path('category/add/<int:parent_id>/', add_folder, name='add-folder'),
 ]
