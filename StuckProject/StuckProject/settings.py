b31e258b147867f25e955840a1f342d72d037715
gahyeon <gimgahyeon12@gmail.com>
2024-06-29 21:44:20 +0900

    Fix: settings.py 수정

--- StuckProject/StuckProject/settings.py
+++ StuckProject/StuckProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import my_settings
+# import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = my_settings.SECRET
+SECRET_KEY = 'django-insecure-k!h1*o-48onx9i47w5#@57j$xti9#q)yb!qwp!v)2q+7g7@^$j',
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
